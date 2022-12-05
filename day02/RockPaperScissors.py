1e56f381352a3005f351ce203fa2671e6fcb4a1c
PhilippSeidel00 <spam@philipp-seidel.de>
2022-12-05 22:22:54 +0100

    solved challenge 2 of day2

--- day02/RockPaperScissors.py
+++ day02/RockPaperScissors.py
@@ -33,3 +33,39 @@ for r in rounds:
 
 print(score)
 
+
+# part two
+decrypted_rounds = []
+
+for i in input:
+    parts = i.split()
+    neededFigure = ''
+    if parts[0] == 'A':
+        if parts[1] == 'X':
+            neededFigure = 'Z'
+        elif parts[1] == 'Y':
+            neededFigure = 'X'
+        else:
+            neededFigure = 'Y'
+    if parts[0] == 'B':
+        if parts[1] == 'X':
+            neededFigure = 'X'
+        elif parts[1] == 'Y':
+            neededFigure = 'Y'
+        else:
+            neededFigure = 'Z'
+    if parts[0] == 'C':
+        if parts[1] == 'X':
+            neededFigure = 'Y'
+        elif parts[1] == 'Y':
+            neededFigure = 'Z'
+        else:
+            neededFigure = 'X'
+
+    decrypted_rounds.append(Round(parts[0], neededFigure))
+
+score = 0
+for r in decrypted_rounds:
+    score += r.score
+
+print(score)
